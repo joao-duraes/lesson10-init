7dfaa460b0f0c6c3cd054d3d4e314ba05630ad12
joao-duraes <joao.a.duraes@gmail.com>
2018-03-08 18:31:46 +0000

    init

---
+++ mainfile.c
@@ -0,0 +1,43 @@
+/*
+ * mainfile.c
+ *
+ *  Created on: 08/03/2018
+ *      Author: aet
+ */
+
+/* Initial source code for lesson 10 exercises
+ *
+ * Instructions
+ * ------------
+ *
+ * Solve each exercise in a separate function
+ * name each function as exerciseN (replace N with exercise number)
+ * call the function of each exercise in function main (one a a time)
+ *
+ * Keep all functions exerciseN (don't erase the code of previous
+ * just keep different names: exercises01   exercise 02 ....)
+ * see example already in this file
+ *
+ *        Note: functions in C will be addressed soon
+ *              for now, just use this scheme to simplify the lesson
+ *
+ * At the end, submit changes to github
+ * some exercises are for practice, some are for project
+ * all should be submitted, however:
+ * -           ->> reserve time for the project <<--
+ *
+ */
+
+/* use this "template" for your functions */
+/* change 0 to the number of the exercise */
+/* keep all functions you wrote */
+void exercise0() {
+	/* do something */
+}
+
+int main() {
+	/* just call your current exercise function (just one at a time) */
+	/* change 0 to the number of the current exercise function */
+	exercise0();
+	return 0;
+}
